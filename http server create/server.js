7997d854182ae257e913229201aa11d0ba772d8e
Gautam Sharma <gautamgaur2004@gamil.com>
2024-09-30 15:12:38 +0530

    first server

---
+++ http server create/server.js
@@ -0,0 +1,18 @@
+// simple http server create
+
+const http = require("http");
+const { hostname } = require("os");
+
+const Port = 3000;
+
+// it is ip addresss of the hostname just for example
+const hostName = '8.8.8.8';
+
+// create the http server Hello Developer
+const server = http.createServer((req, res) => {
+  res.end("Hello Developer");
+});
+
+server.listen(Port, () => {
+  console.log(`Your server is running at http://${hostName}:${Port}`);
+});
